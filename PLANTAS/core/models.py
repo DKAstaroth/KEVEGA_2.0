3764e5f313c838569b65a4b983ef72f4c10bfe0a
JuanVega <vegalagos.jpa@gmail.com>
2024-06-26 16:25:12 -0400

    3.9
    registra usuarios a sqlite

--- PLANTAS/core/models.py
+++ PLANTAS/core/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)  # Debes considerar el uso seguro de contraseñas
 
-#class Figura(models.Model):
- #   altura = models.IntegerField()
-  #  valor = models.IntegerField()
-   # nombre= models.CharField(max_length=50)
-   # def __str__(self):
-   #     return self.nombre
-    
+    def __str__(self):
+        return self.username
 
 
